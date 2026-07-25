10c967c1cd6b8f74950aea92eac23ec9382a4a89
Raunak S <raunakshaikh1203@gmail.com>
2026-07-25 15:25:33 +0530

    Time: 7 ms (13.7%), Space: 8.7 MB (37.71%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    int reverse(int n) {
+        int revnum = 0 ; 
+
+        while(n!=0){
+            int dig = n%10 ; 
+
+            if(revnum > INT_MAX/10 || revnum < INT_MIN/10){
+                return 0 ; 
+            }
+
+            revnum = (revnum*10) + dig ; 
+            n = n/10 ;
+        }
+
+        return revnum ; 
+    }
+
+    bool isPalindrome(int x) {
+        if (x < 0) return 0 ; 
+        int revnum = reverse(x) ; 
+        return revnum == x ; 
+    }
+};
