ce46aeaf1e6aa396b0c6a2600eaeebc55dd9c0c2
Raunak S <raunakshaikh1203@gmail.com>
2026-08-10 22:08:56 +0530

    Time: 34 ms (73.12%), Space: 9 MB (73.64%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+         vector<bool>t(n+1,false);
+        for(int i=0;i<n+1;i++){
+            for(int k=1;k*k<=i;k++){
+                if(t[i-(k*k)]==false){
+                    t[i]=true;
+                    break;
+                }
+            }
+        }
+        return t[n];
+    }
+};
